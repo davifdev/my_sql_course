051eb7cda50b81c2519849a41e1e3eaea655ecc7
Davi Fernandes <davi.fer159@gmail.com>
2026-06-01 10:32:21 -0300

    Revert "fix: remove contratint nonused"
    This reverts commit 73525df56683593b9414cf1cb5c2914a432fe661.

--- 01-criando-tabelas-colunas.sql
+++ 01-criando-tabelas-colunas.sql
@@ -6,6 +6,7 @@ CREATE TABLE base_de_dados.users (
 	password_hash varchar(100) NOT NULL,
 
   CONSTRAINT users_unique UNIQUE KEY (email);
+  CONSTRAINT users_unique_1 UNIQUE KEY (password_hash)
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
@@ -16,7 +17,7 @@ CREATE TABLE base_de_dados.profiles (
 	user_id INTEGER NOT NULL,
 
 	CONSTRAINT profiles_pk PRIMARY KEY (id),
-	CONSTRAINT profiles_users_FK FOREIGN KEY (user_id) REFERENCES base_de_dados.profiles(id)
+	CONSTRAINT profiles_users_FK FOREIGN KEY (user_id) REFERENCES base_de_dados.profiles(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE base_de_dados.roles (
@@ -29,7 +30,7 @@ CREATE TABLE base_de_dados.roles (
 CREATE TABLE base_de_dados.users_roles (
 	user_id INTEGER NOT NULL,
 	role_id INTEGER NOT NULL,
-
+  
 	CONSTRAINT users_roles_pk PRIMARY KEY (user_id,role_id),
 	CONSTRAINT users_roles_users_FK FOREIGN KEY (user_id) REFERENCES base_de_dados.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT users_roles_roles_FK FOREIGN KEY (role_id) REFERENCES base_de_dados.roles(id) ON DELETE CASCADE ON UPDATE CASCADE
